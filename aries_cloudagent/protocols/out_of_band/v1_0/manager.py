5716ecbe8d17ef9b71d5b3cc0b5397c854268565
ChangHyunkmov <hot9998@kmov.kr>
2020-10-05 15:06:00 +0900

    Fixed Out of Band receive invitation function

--- aries_cloudagent/protocols/out_of_band/v1_0/manager.py
+++ aries_cloudagent/protocols/out_of_band/v1_0/manager.py
@@ -1,14 +1,16 @@
 """Classes to manage connections."""
 
 import logging
+import json
 
 from ....connections.models.connection_record import ConnectionRecord
 from ....config.injection_context import InjectionContext
 from ....core.error import BaseError
 from ....ledger.base import BaseLedger
-from ....wallet.util import did_key_to_naked, naked_to_did_key
+from ....wallet.util import did_key_to_naked, naked_to_did_key, b64_to_str
 
 from ...connections.v1_0.manager import ConnectionManager
+from ...present_proof.v1_0.manager import PresentationManager
 from ...connections.v1_0.messages.connection_invitation import ConnectionInvitation
 from ...didcomm_prefix import DIDCommPrefix
 from ...issue_credential.v1_0.models.credential_exchange import V10CredentialExchange
@@ -17,6 +19,7 @@ from ...present_proof.v1_0.models.presentation_exchange import V10PresentationEx
 from .models.invitation import Invitation as InvitationModel
 from .messages.invitation import Invitation as InvitationMessage
 from .messages.service import Service as ServiceMessage
+
 
 HS_PROTO_CONN_INVI = "connections/1.0/invitation"
 
@@ -186,6 +189,8 @@ class OutOfBandManager:
 
         # New message format
         invitation_message = InvitationMessage.deserialize(invitation)
+        print("-" * 10, "invitation_message", "-" * 10)
+        print("invitation_message", invitation_message)
 
         # Convert to old format and pass to relevant manager
         # The following logic adheres to Aries RFC 0496
@@ -205,10 +210,14 @@ class OutOfBandManager:
         else:
             # If it's in the did format, we need to convert to a full service block
             service_did = invitation_message.service_dids[0]
+            print("-" * 10, "service_did", "-" * 10)
+            print(service_did)
             async with ledger:
                 verkey = await ledger.get_key_for_did(service_did)
                 did_key = naked_to_did_key(verkey)
                 endpoint = await ledger.get_endpoint_for_did(service_did)
+            print("-" * 10, "did_", "-" * 10)
+            print(verkey, did_key, endpoint)
             service = ServiceMessage.deserialize(
                 {
                     "id": "#inline",
@@ -218,11 +227,14 @@ class OutOfBandManager:
                     "serviceEndpoint": endpoint,
                 }
             )
+            print("-" * 10, "serivce", "-" * 10)
+            print(service)
 
         unq_handshake_protos = {
             DIDCommPrefix.unqualify(proto)
             for proto in invitation_message.handshake_protocols
         }
+
         # If we are dealing with an invitation
         if unq_handshake_protos == {HS_PROTO_CONN_INVI}:
             if len(invitation_message.request_attach) != 0:
@@ -250,6 +262,9 @@ class OutOfBandManager:
                 }
             )
 
+            print("-" * 10, "connection_invitation", "-" * 10)
+            print(connection_invitation)
+
             connection_mgr = ConnectionManager(self.context)
             connection = await connection_mgr.receive_invitation(
                 connection_invitation, auto_accept=True
@@ -261,11 +276,69 @@ class OutOfBandManager:
             pfx.qualify("present-proof/1.0/request-presentation")
             for pfx in DIDCommPrefix
         ]:
-            raise OutOfBandManagerNotImplementedError(
-                f"{invitation_message.request_attach[0].data.json['@type']} "
-                "request type not implemented."
+            # raise OutOfBandManagerNotImplementedError(
+            #     f"{invitation_message.request_attach[0].data.json['@type']} "
+            #     "request type not implemented."
+            # )
+            service.recipient_keys = [
+                did_key_to_naked(key) for key in service.recipient_keys or []
+            ]
+            print("-" * 10, "service.recipient_keys", "-" * 10)
+            print(service.recipient_keys)
+            service.routing_keys = [
+                did_key_to_naked(key) for key in service.routing_keys
+            ] or []
+
+            # Convert to the old message format
+            connection_invitation = ConnectionInvitation.deserialize(
+                {
+                    "@id": invitation_message._id,
+                    "@type": DIDCommPrefix.qualify_current(HS_PROTO_CONN_INVI),
+                    "label": invitation_message.label,
+                    "recipientKeys": service.recipient_keys,
+                    "serviceEndpoint": service.service_endpoint,
+                    "routingKeys": service.routing_keys,
+                }
             )
+
+            print("-" * 10, "connection_invitation", "-" * 10)
+            print(connection_invitation)
+
+            connection_mgr = ConnectionManager(self.context)
+            connection = await connection_mgr.receive_invitation(
+                connection_invitation, auto_accept=True
+            )
+            print("-"*10, "connection_id", "-"*10)
+            print(connection._id)
+
+            request_attach = invitation_message.request_attach
+            print("-" * 10, "request_attach", "-" * 10)
+            print(request_attach)
+
+            request_attach_data = request_attach[0].data.json_
+            print("-" * 10, "request_attach_data", "-" * 10)
+            print(request_attach_data)
+
+            proof_message = V10PresentationExchange(
+                thread_id=request_attach_data["@id"],
+                state=V10PresentationExchange.STATE_REQUEST_RECEIVED,
+                presentation_request=json.loads(b64_to_str(
+                    request_attach_data["request_presentations~attach"][0]["data"]["base64"])),
+                connection_id=connection._id,
+            )
+
+            print("-" * 10, "proof_message", "-" * 10)
+            print("proof_message", proof_message)
+            presentation_manager = PresentationManager(self.context)
+            pre_ex_id = await presentation_manager.receive_request(
+                presentation_exchange_record=proof_message)
+            print("-" * 10, "pre_ex_id", "-" * 10)
+            print(pre_ex_id)
         else:
+            print([
+                pfx.qualify("present-proof/1.0/request-presentation")
+                for pfx in DIDCommPrefix
+            ])
             raise OutOfBandManagerError("Invalid request type")
 
         return connection
